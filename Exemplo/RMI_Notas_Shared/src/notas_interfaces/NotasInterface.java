5083daccab6e8a4988a2aea366a3a91f2cd66482
GabrielRubin <gabriel_rubin@hotmail.com>
2015-09-30 02:27:45 -0300

    first commit
    start

---
+++ Exemplo/RMI_Notas_Shared/src/notas_interfaces/NotasInterface.java
@@ -0,0 +1,11 @@
+package notas_interfaces;
+// Arquivo: NotasInterface.java (Roland Teodorowitsch; 28 ago. 2013)
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface NotasInterface extends Remote {
+	public double obtemNota(String nome) throws RemoteException;
+	
+	void mudaNota(String nome) throws RemoteException;
+}
+
